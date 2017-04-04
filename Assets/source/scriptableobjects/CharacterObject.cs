8388a839e37ab520310db45bd2cac09c025d62d8
Vidsneezes <moraleszez@gmail.com>
2017-04-04 13:53:26 -0600

    added character profile  scriptable object, import text mesh pro

---
+++ Assets/source/scriptableobjects/CharacterObject.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Character Profile", menuName = "SwiperEngine/CharacterProfile",order = 0)]
+public class CharacterObject : ScriptableObject {
+    public List<EmotionStruct> emotions;
+}
+
+[System.Serializable]
+public struct EmotionStruct
+{
+    public string name;
+    public Sprite sprite;
+    public Vector2 offset;
+    public Color color;
+}
