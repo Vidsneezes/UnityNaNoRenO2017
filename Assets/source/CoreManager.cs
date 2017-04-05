774c5c37e8b36936d559a412813689fb47f8b82f
Vidsneezes <moraleszez@gmail.com>
2017-04-04 22:37:37 -0600

    added coremanager to scene

--- Assets/source/CoreManager.cs
+++ Assets/source/CoreManager.cs
@@ -9,5 +9,27 @@ namespace SwiperEngine
     {
         public CoreLogic coreLogic;
 
+        public DialoguePanel leftPanelPrefab;
+        public DialoguePanel rightPanelPrefab;
+        public Transform inactivePanelsHolder;
+        public Transform activePanelsHolder;
+
+        public float panelHeight;
+        public float panelPadding;
+
+        private List<DialoguePanel> activePanels;
+        private List<DialoguePanel> inactivePanels;
+
+        private void Start()
+        {
+            activePanels = new List<DialoguePanel>();
+            inactivePanels = new List<DialoguePanel>();
+        }
+
+        public void SpawnPanel()
+        {
+
+        }
+
     }
 }
--- Assets/source/UI/DialoguePanel.cs
+++ Assets/source/UI/DialoguePanel.cs
@@ -3,19 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class DialoguePanel : MonoBehaviour {
-
-    public Image characterSprite;
-    public Image emotionPanel;
-
-    public void ChangeCharacterSprite(Sprite newSprite)
+namespace SwiperEngine
+{
+    public class DialoguePanel : MonoBehaviour
     {
-        characterSprite.sprite = newSprite;
-        //Reset width
-    }
 
-    public void ChangeEmotionSpriteColor(Color color)
-    {
-        emotionPanel.color = color;
+        public Image characterSprite;
+        public Image emotionPanel;
+
+        public void ChangeCharacterSprite(Sprite newSprite)
+        {
+            characterSprite.sprite = newSprite;
+            //Reset width
+        }
+
+        public void ChangeEmotionSpriteColor(Color color)
+        {
+            emotionPanel.color = color;
+        }
     }
 }
