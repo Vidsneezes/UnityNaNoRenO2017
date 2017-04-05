44cb819de0cbe8b568a6f77a29a2cd74ec12924f
Vidsneezes <moraleszez@gmail.com>
2017-04-04 23:19:36 -0600

    added dotween library

--- Assets/source/CoreManager.cs
+++ Assets/source/CoreManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace SwiperEngine
 {
@@ -16,6 +17,7 @@ namespace SwiperEngine
 
         public float panelHeight;
         public float panelPadding;
+        public Image backgroundImage;
 
         private List<DialoguePanel> activePanels;
         private List<DialoguePanel> inactivePanels;
@@ -25,23 +27,30 @@ namespace SwiperEngine
             activePanels = new List<DialoguePanel>();
             inactivePanels = new List<DialoguePanel>();
             SpawnPanel();
-            SpawnPanel();
-            SpawnPanel();
-            SpawnPanel();
-            SpawnPanel();
-            SpawnPanel();
-            SpawnPanel();
-            SpawnPanel();
-
         }
 
         public void SpawnPanel()
         {
             DialoguePanel dp = GameObject.Instantiate(leftPanelPrefab);
             dp.transform.SetParent(activePanelsHolder);
-            dp.transform.localPosition = new Vector3(dp.transform.localPosition.x,activePanels.Count * panelHeight + panelPadding );
             dp.transform.localScale = new Vector3(1, 1, 1);
+            dp.transform.localPosition = new Vector3(0, -300, 0);
+            Vector3 placePosition = new Vector3(0, activePanels.Count * panelHeight + panelPadding);
+            dp.transform.DOLocalMove(placePosition, 0.3f).SetEase(Ease.InBounce);
             activePanels.Add(dp);
+        }
+
+        public void ShiftOtherPanelsUp()
+        {
+            for (int i = 0; i < activePanels.Count; i++)
+            {
+
+            }
+        }
+     
+        private void ShiftPanel()
+        {
+
         }
 
     }
