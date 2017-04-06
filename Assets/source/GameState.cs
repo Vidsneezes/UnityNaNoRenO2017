e342ff2bb3965ac24e7bb654fc78a15a59a551fa
Vidsneezes <moraleszez@gmail.com>
2017-04-06 13:35:28 -0600

    added boot , mainmenu, credits scene, added a boot loader and created a Game State cs

---
+++ Assets/source/GameState.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameState : MonoBehaviour {
+
+    public string state;
+    public string activeScene;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	private void Update () {
+      
+	}
+
+
+
+    public void ToMainMenu()
+    {
+        SceneManager.UnloadSceneAsync(activeScene);
+        activeScene = "MainMenu";
+    }
+
+    IEnumerator LoadScenAsyncRoutine(string sceneName, string futureState)
+    {
+        AsyncOperation asyncOpDone = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+        while (!asyncOpDone.isDone)
+        {
+            yield return new WaitForEndOfFrame();
+        }
+        //loadCamera.gameObject.SetActive(false);
+        state = futureState;
+
+    }
+}
