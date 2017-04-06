a70857489a8676d7157c3af10bafa1bfd24aa430
Vidsneezes <moraleszez@gmail.com>
2017-04-06 14:28:41 -0600

    added tweens to main menu ui

--- Assets/source/MainMenuScript.cs
+++ Assets/source/MainMenuScript.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 namespace SwiperEngine
 {
@@ -11,14 +12,19 @@ namespace SwiperEngine
         public Button instructionButton;
         public Button cancelButtons;
 
+        public Image background;
+
         public GameObject instructionsPanel;
 
         private void Awake()
         {
             instructionsPanel.SetActive(false);
             startGameButton.onClick.AddListener(ToGame);
+            startGameButton.transform.DOLocalMoveY(1.2f, 0.3f).SetRelative().SetLoops(-1, LoopType.Yoyo);
             instructionButton.onClick.AddListener(ViewInstructions);
+            instructionButton.transform.DOLocalMoveY(2.5f, 0.2f).SetRelative().SetLoops(-1, LoopType.Yoyo);
             cancelButtons.onClick.AddListener(CloseInstructions);
+            background.transform.DOScale(new Vector3(0.03f, 0.03f, 0.03f), 1.2f).SetRelative().SetLoops(-1, LoopType.Yoyo);
         }
 
         public void ToGame()
