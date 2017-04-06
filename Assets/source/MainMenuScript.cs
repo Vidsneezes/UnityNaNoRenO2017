edf233945492e5c9b4ea99ece975ff4117f5d8c8
Vidsneezes <moraleszez@gmail.com>
2017-04-06 14:23:50 -0600

    added callback to buttons in mainmenuscript

---
+++ Assets/source/MainMenuScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SwiperEngine
+{
+    public class MainMenuScript : MonoBehaviour
+    {
+        public Button startGameButton;
+        public Button instructionButton;
+        public Button cancelButtons;
+
+        public GameObject instructionsPanel;
+
+        private void Awake()
+        {
+            instructionsPanel.SetActive(false);
+            startGameButton.onClick.AddListener(ToGame);
+            instructionButton.onClick.AddListener(ViewInstructions);
+            cancelButtons.onClick.AddListener(CloseInstructions);
+        }
+
+        public void ToGame()
+        {
+            startGameButton.onClick.RemoveAllListeners();
+            GameState.instance.ToCoreScene();
+        }
+
+        public void ViewInstructions()
+        {
+            instructionsPanel.gameObject.SetActive(true);
+        }
+
+        public void CloseInstructions()
+        {
+            instructionsPanel.gameObject.SetActive(false);
+        }
+    }
+}
